88dcb103db093033194d176f9c38d9dd625703b8
Rj657932 <lopezmedinaronaldjose@gmail.com>
2026-05-14 21:38:31 -0500

    master - update project and add root directory

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./database/database')
+const db = require('./src/database/database');
 const app = express();
 
 /* ARCHIVOS PUBLICOS */
